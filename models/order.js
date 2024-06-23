4a790eecd06976b1b74fdfdcf3413903a11e38df
Sage9705 <edem22kumahor@gmail.com>
2024-06-23 21:51:01 +0100

    Define Order Schema

---
+++ models/order.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const OrderSchema = new mongoose.Schema({
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    products: [{
+        product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
+        quantity: { type: Number, required: true }
+    }],
+    total: { type: Number, required: true },
+    status: { type: String, enum: ['pending', 'shipped', 'delivered', 'cancelled'], default: 'pending' },
+    createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('Order', OrderSchema);
