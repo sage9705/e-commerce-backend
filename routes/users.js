3ddb30afc2ccd7239f2d181ab616ad5946b0dea6
Sage9705 <edem22kumahor@gmail.com>
2024-07-09 23:11:27 +0100

    Add email verification route

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser, getUserProfile } = require('../controllers/UserController');
+const { registerUser, loginUser, getUserProfile, verifyEmail } = require('../controllers/UserController');
 const { verifyToken } = require('../middlewares/auth');
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.get('/profile', verifyToken, getUserProfile);
+router.get('/verify/:token', verifyEmail);
 
 module.exports = router;
