d393853f27abc3568946b7c8b71b831e4796c45c
Sage9705 <edem22kumahor@gmail.com>
2024-08-14 17:29:47 +0100

    Implement validation for users

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,36 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser, getUserProfile, verifyEmail } = require('../controllers/UserController');
-const { verifyToken } = require('../middlewares/auth');
+const { body } = require("express-validator");
+const {
+  registerUser,
+  loginUser,
+  getUserProfile,
+  verifyEmail,
+} = require("../controllers/UserController");
+const { verifyToken } = require("../middlewares/auth");
+const validate = require("../middlewares/validate");
 
-router.post('/register', registerUser);
-router.post('/login', loginUser);
-router.get('/profile', verifyToken, getUserProfile);
-router.get('/verify/:token', verifyEmail);
+const userValidationRules = [
+  body("name")
+    .trim()
+    .isLength({ min: 2 })
+    .withMessage("Name must be at least 2 characters long"),
+  body("email").isEmail().withMessage("Must be a valid email address"),
+  body("password")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters long"),
+];
+
+const loginValidationRules = [
+  body("email").isEmail().withMessage("Must be a valid email address"),
+  body("password")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters long"),
+];
+
+router.post("/register", userValidationRules, validate, registerUser);
+router.post("/login", loginValidationRules, validate, loginUser);
+router.get("/profile", verifyToken, getUserProfile);
+router.get("/verify/:token", verifyEmail);
 
 module.exports = router;
